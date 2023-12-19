7f66e17412a74cc7c4401e7a90574a99fe8a9ed2
yllorca <yllorca@helloworld.cl>
2023-12-19 17:28:17 -0300

    fix celery

--- web_k8s/celery/__init__.py
+++ web_k8s/celery/__init__.py
@@ -5,7 +5,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'immedic.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_k8s.settings')
 
 app = Celery('web_k8s')
 
