27c5e92032160d390f3ff4119b882d3421f97afd
yllorca <yllorca@helloworld.cl>
2023-12-20 08:18:27 -0300

    add new item to post model

--- post/models.py
+++ post/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
